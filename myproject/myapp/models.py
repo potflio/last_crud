7b5cd27e8a5d2b22ebfa4f06d005ab84a360ceeb
PrakashPandian <120122316+potflio@users.noreply.github.com>
2023-12-01 05:29:45 +0530

    Update models.py

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    
+    class Meta:  
+        db_table = "student"
